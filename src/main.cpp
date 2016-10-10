da092656dd93ef586f5b73ae8b4d5c7115a446cd
Aleš Katona <ales@mymobilecoverage.com>
2016-10-10 16:58:04 -0600

    bump version for geth update

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Etherdyne");
     QCoreApplication::setOrganizationDomain("etherwall.com");
     QCoreApplication::setApplicationName("Etherwall");
-    QCoreApplication::setApplicationVersion("1.4.3");
+    QCoreApplication::setApplicationVersion("1.4.4");
     app.setWindowIcon(QIcon(QPixmap(":/images/icon")));
 
     QTranslator translator;

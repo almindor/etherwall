ef291bdc69cea92e35816c2b652959ad3a6bfa8e
Aleš Katona <ales@mymobilecoverage.com>
2017-08-13 21:16:38 +0200

    * create proto subdir on generate script

--- generate_protobuf.sh
+++ generate_protobuf.sh
@@ -2,6 +2,7 @@
 CURDIR=$(pwd)
 
 cd $CURDIR/src/trezor/trezor-common/protob
+mkdir -p "$CURDIR/src/trezor/proto"
 
 for i in messages types config storage ; do
     protoc --cpp_out=$CURDIR/src/trezor/proto/ -I/usr/include -I. $i.proto

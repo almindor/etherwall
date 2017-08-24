29e89e36b1ae05c6daf792950f78ad672af684e9
Ales Katona <almindor@gmail.com>
2017-08-24 13:31:39 -0600

    * allow replacing of dir variables in generate script

--- generate_protobuf.sh
+++ generate_protobuf.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 CURDIR=$(pwd)
+DESTDIR="$CURDIR/src/trezor/proto"
+SRCDIR="$CURDIR/src/trezor/trezor-common/protob"
 
-cd $CURDIR/src/trezor/trezor-common/protob
-mkdir -p "$CURDIR/src/trezor/proto"
+mkdir -p "$DESTDIR"
+cd "$SRCDIR"
 
 for i in messages types config storage ; do
-    protoc --cpp_out=$CURDIR/src/trezor/proto/ -I/usr/include -I. $i.proto
+    protoc --cpp_out="$DESTDIR" -I/usr/include -I. $i.proto
 done

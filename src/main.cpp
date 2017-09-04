ad04c5741fb4a4ac24d98fd2911ac82147c6deba
Aleš Katona <ales@mymobilecoverage.com>
2017-09-04 09:53:22 -0600

    * use version 2.0.4 for a quickfix release

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Etherdyne");
     QCoreApplication::setOrganizationDomain("etherwall.com");
     QCoreApplication::setApplicationName("Etherwall");
-    QCoreApplication::setApplicationVersion("2.1.0");
+    QCoreApplication::setApplicationVersion("2.0.4");
     app.setWindowIcon(QIcon(QPixmap(":/images/icon")));
 
     QTranslator translator;

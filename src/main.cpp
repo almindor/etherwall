c8996668f883efca5012d3f0092e9085cf5d9c65
Ales Katona <almindor@gmail.com>
2018-05-07 20:01:34 -0600

    bump version to v2.2.2

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Etherdyne");
     QCoreApplication::setOrganizationDomain("etherwall.com");
     QCoreApplication::setApplicationName("Etherwall");
-    QCoreApplication::setApplicationVersion("2.2.1");
+    QCoreApplication::setApplicationVersion("2.2.2");
     app.setWindowIcon(QIcon(QPixmap(":/images/icon")));
 
     QTranslator translator;

a0dbc3639c2f07aec8c78a21705c5dc64d895a7f
Ales Katona <ales@katona.me>
2021-01-05 09:47:07 -0800

    upgrade ew-node fixing token balance bug

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,7 @@
 using namespace Etherwall;
 
 // ew-node version check
-#if EW_NODE_VERSION != 1000006
+#if EW_NODE_VERSION != 1000007
 #error "ew-node version mismatch, update git submodules"
 #endif
 

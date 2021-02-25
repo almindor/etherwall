f406163940ed777b6f2836e45b3f7963fe2d825e
Ales Katona <ales@katona.me>
2021-02-25 14:15:30 -0800

    bump to v3.0.2

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Etherdyne");
     QCoreApplication::setOrganizationDomain("etherwall.com");
     QCoreApplication::setApplicationName("Etherwall");
-    QCoreApplication::setApplicationVersion("3.0.1");
+    QCoreApplication::setApplicationVersion("3.0.2");
     app.setWindowIcon(QIcon(QPixmap(":/images/icon")));
 
     QTranslator translator;
